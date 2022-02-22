2b48baa6df225fbce9735c9592c2e7ed221e89fd
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2022-02-21 20:07:21 -0600

    final round of conosl.log clean up

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -2,7 +2,7 @@ const $animalForm = document.querySelector("#animals-form");
 const $displayArea = document.querySelector("#display-area");
 
 const printResults = (resultArr) => {
-  console.log(resultArr);
+  //console.log(resultArr);
 
   const animalHTML = resultArr.map(
     ({ id, name, personalityTraits, species, diet }) => {
@@ -66,8 +66,9 @@ const handleGetAnimalsSubmit = (event) => {
   }
 
   const personalityTraitArr = [];
-  const selectedTraits = $animalForm.querySelector('[name="personality"')
-    .selectedOptions;
+  const selectedTraits = $animalForm.querySelector(
+    '[name="personality"'
+  ).selectedOptions;
 
   for (let i = 0; i < selectedTraits.length; i += 1) {
     personalityTraitArr.push(selectedTraits[i].value);
